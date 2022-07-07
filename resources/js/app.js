0ae898b3679e982bcfab797785e4c6bb589905bf
Flash7227 <punk_lzi23@yahoo.com>
2022-07-07 12:46:03 +0800

    testing

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,23 @@
+//Intro
 require('./bootstrap');
-
 window.Vue = require('vue');
+
+//dependencies will place below
 import ViewUI from 'view-design';
 import locale from 'view-design/dist/locale/en-US';
 import 'view-design/dist/styles/iview.css';
 Vue.use(ViewUI, { locale });
 
+window.moment = require('moment'); 
+
+
+
+//components will place below
+Vue.component('navbar-comp', require('./components/navbar.vue').default);
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 
+//frame
 const app = new Vue({
     el: '#app',
 });

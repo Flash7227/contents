8846d95385f798cc3ebc3fb5ff3e1586bd51ad98
Flash7227 <punk_lzi23@yahoo.com>
2022-07-08 17:44:54 +0800

    navchange

--- resources/js/components/navbar.vue
+++ resources/js/components/navbar.vue
@@ -1,32 +1,32 @@
 <template>
     <div>
         <Menu mode="horizontal" theme="primary" active-name="1">
-            <MenuItem name="1">
+            <!-- <MenuItem name="1">
                 <Icon type="ios-paper" />
                 内容管理
-            </MenuItem>
+            </MenuItem> -->
             <MenuItem name="2">
-                <Icon type="ios-people" />
-                用户管理
+                <Icon type="md-home" />
+                Нүүр
             </MenuItem>
             <Submenu name="3">
                 <template slot="title">
-                    <Icon type="ios-stats" />
-                    统计分析
+                    <Icon type="md-image" />
+                    Контент
                 </template>
-                <MenuGroup title="使用">
-                    <MenuItem name="3-1">新增和启动</MenuItem>
-                    <MenuItem name="3-2">活跃分析</MenuItem>
-                    <MenuItem name="3-3">时段分析</MenuItem>
+                <MenuGroup title="Үндсэн">
+                    <MenuItem name="3-1">Нийтлэл</MenuItem>
+                    <MenuItem name="3-2">Бичлэг</MenuItem>
+                    <MenuItem name="3-3">Постер</MenuItem>
                 </MenuGroup>
-                <MenuGroup title="留存">
-                    <MenuItem name="3-4">用户留存</MenuItem>
-                    <MenuItem name="3-5">流失用户</MenuItem>
+                <MenuGroup title="Бусад">
+                    <MenuItem name="3-4">MNB TV</MenuItem>
+                    <MenuItem name="3-5">MNB RADIO</MenuItem>
                 </MenuGroup>
             </Submenu>
-            <MenuItem name="4">
-                <Icon type="ios-construct" />
-                综合设置
+            <MenuItem name="4" to="/test">
+                <Icon type="md-help" />
+                Бидний тухай
             </MenuItem>
         </Menu>
     </div>

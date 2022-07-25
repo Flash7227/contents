e8bb143d740b9589e7d258860e8f611d40800c97
Flash7227 <punk_lzi23@yahoo.com>
2022-07-25 17:14:40 +0800

    nullable added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('role')->default(0);
-            $table->json('permissions');
+            $table->json('permissions')->nullable();
             $table->string('storage_limit')->default("0");
             $table->string('avatar')->nullable();
             $table->rememberToken();

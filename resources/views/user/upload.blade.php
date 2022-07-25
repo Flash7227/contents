eaffe6b7cf06b5667b7a04ac2034fb3e302c3e19
Flash7227 <punk_lzi23@yahoo.com>
2022-07-25 09:21:20 +0800

    breadcrumb added

--- resources/views/user/upload.blade.php
+++ resources/views/user/upload.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
 @section('content')
+<breadcrumb :urls="['ҮНДСЭН', 'ФАЙЛ ХУУЛАХ']"></breadcrumb>
 <upload-comp csrf="{{ csrf_token() }}" states="{{$emails}}" user="{{Auth()->user()}}"></upload-comp>
 @endsection
